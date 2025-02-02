3e178c0a21f1501e0443bff26d87ae467964308e
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-02-02 23:48:59 +0000

    M1T2

---
+++ M1T2_JobesShane.cpp
@@ -0,0 +1,14 @@
+// CSC 134
+// M1T2
+// Shane Jobes
+// 02/02/2025
+
+#include <iostream>;
+using namespace std;
+
+int main()
+{
+    cout <<"Shane Jobes" << endl;
+    cout <<"Enrolled in the computer programmer program at FTCC" << endl;
+    cout <<"I work for a VA subcontractor repairing power wheelchairs" << endl;
+}

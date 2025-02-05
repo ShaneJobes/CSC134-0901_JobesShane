40222e52f83c6524514ffefd5f9dc0cf2c65570a
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-02-05 01:06:13 +0000

    M1Lab_JobesShane

---
+++ M1Lab_JobesShane.cpp
@@ -0,0 +1,31 @@
+// CSC 134
+// M1LAB
+// Shane Jobes
+// 02/04/2025
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    // This program will simulate an apple orchard.
+    // The owner’s name
+    string name = “Shane Jobes”
+
+    // number of apples owned
+    int apples = 100
+
+    // price per apple
+    double pricePerApple = 0.25
+
+    // calculate the total price of the apples
+    // TODO
+    double totalPrice;
+    totalPrice = apples  * pricePerApple;
+    // print all the information about the orchard
+    // TODO
+    cout << “Welcome to “ << name;
+    cout << “ ’s apple orchard.” << endl;
+    cout << "We have " << apples << "available." << endl;
+    cout << "They are $" << pricePerApple << "per apple" << endl;
+    cout << "If you want all of them it is $" << totalPrice << endl;
+}

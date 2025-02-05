5bb291a83a0779b40a64f4982f4736e1fb2c500d
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-02-05 01:31:52 +0000

    M1HW_JobesShane

---
+++ M1HW_JobesShane.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M1LAB
+// Shane Jobes
+// 02/04/2025
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string title = "Happy Gilmore";
+    int releaseYear = 1996;
+    double boxOffice = 41.2; //box office is in millions
+
+    cout << "The Adam Sandler classic " << title << " came out in " << releaseYear << "." << endl;
+    cout << "At the box office this film brought in " << boxOffice << " million dollars." << endl;
+    cout << "This film was known for it's hilarious one liners, including:" << endl;
+    cout << "\"Did that go in? I wasn’t watching, did it go in? I didn’t see it, could you tell me if it went in?\"" << endl;
+    cout << "\"Are you too good for your home? Answer me! Suck my white a**, ball!\"" << endl;
+    cout << "\"You could trouble me for a warm glass of shut the hell up!\"" << endl;
+    cout << "\"You’re in big trouble though, pal. I eat pieces of s*** like you for breakfast!\"" << endl;
+    cout << "\"Oh, she got hit by a car. She's dead.\"" << endl;
+}

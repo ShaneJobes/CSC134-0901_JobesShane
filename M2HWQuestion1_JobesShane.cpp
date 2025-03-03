671eb1a42bfc6e05edf0f0366e10a0fa0d933417
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-03-03 01:20:14 +0000

    M2HWQuestion1_JobesShane

---
+++ M2HWQuestion1_JobesShane.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M2HW Qustion 1 - Gold
+// 03/02/2025
+// Shane Jobes
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+  
+int main()
+{
+    //Declare variables
+    string userName;
+    double startingBalance, deposit, withdrawal, finalBalance;
+    int accountNum;
+
+    //Get user input to get values for variables
+    cout << "What is your name?" << endl;
+    cin >> userName;
+    cout << "What is your acocunt number?" << endl;
+    cin >> accountNum;
+    cout << "How much was in the bank account at the start of the month?" << endl;
+    cin >> startingBalance;
+    cout << "How much did you deposit and how much did you withdrawal throughout the month?" << endl;
+    cin >> deposit >> withdrawal;
+
+    //Calculate balance
+    finalBalance = startingBalance + deposit - withdrawal;
+    cout << setprecision(2) << fixed << "Youre final balance is $" << finalBalance << " for account #" << accountNum << endl;
+
+    return 0;
+}
--- M2HW_JobesShane.cpp
+++
@@ -1,4 +0,0 @@
-// CSC 134
-// M2HW - Gold
-// 02/23/2025
-// Shane Jobes

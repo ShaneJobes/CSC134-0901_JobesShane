af7e65148c3db6433b8ce6c364b6e36a6dd383fe
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-03-10 00:05:31 +0000

    M3HW_Gold_JobesShane

--- M3HW_Gold_JobesShane.cpp
+++ M3HW_Gold_JobesShane.cpp
@@ -72,10 +72,97 @@ void question2()
 }
 void question3() 
 {
-    
+    //Declare variables
+    string wantToPlay;
+    string choice1;
+    string choice2;
+    string choice3;
+
+    //Opening to game
+    cout << "Welcome to Adventureland, all choices should be answsered with a y for yes and an n for no!" << endl;
+    cout << "Would you like to play? ";
+    cin >> wantToPlay;
+
+    //Check for user input then move to next part of game based on input
+    if (wantToPlay == "y")
+    {
+        cout << "You start in a forest searching for food to feed your family. Suddenly you are overwhelmed with the sounds of battle." << endl;
+        cout << "Do you make your way towards the fighting(y) or run away(n)? ";
+        cin >> choice1;
+        if (choice1 == "y")
+        {
+            cout << "You come upon a merchant's caravan being attacked by some bandits!" << endl;
+            cout << "Do you help the caravan? ";
+            cin >> choice2;
+            if (choice2 == "n")
+            {
+                cout << "You watch the caravan guards barely fend off the bandits, ending the battle with heavy casualties. Do you finish what they started? ";
+                cin >> choice3;
+                if (choice3 == "n")
+                {
+                    cout << "You move in to help the caravan leave with their wounded and they give you some supplies for your effort. Now you'll be able to feed your family for weeks." << endl;
+                    cout << "Congrats you've won!" << endl;
+                }
+                else if (choice3 == "y")
+                {
+                    cout << "You don't know how to use a sword! Your first swing bounces of the guards armor and you are quickly taken down and arrested. At least your family will have closure at your hanging." << endl;
+                    cout << "GAME OVER" << endl;
+                }
+                else
+                {
+                    cout << "You know you were supposed to enter 'y' or 'n' right..." << endl;
+                }
+            }
+            else if (choice2 == "y")
+            {
+                cout << "You are no fighter, you run in to help the guards but are quickly cut down by the bandits." << endl;
+                cout << "GAME OVER" << endl;
+            }
+            else
+        {
+            cout << "You know you were supposed to enter 'y' or 'n' right..." << endl;
+        }
+        }
+        else if (choice1 == "n")
+        {
+            cout << "You are hit by a stray musket shot, your family will never know your fate" << endl;
+            cout << "GAME OVER" << endl;
+        }
+        else
+        {
+            cout << "You know you were supposed to enter 'y' or 'n' right..." << endl;
+        }
+    }
+    else if (wantToPlay == "n")
+    {
+        cout << "Then move along so somebody who does can get a turn!" << endl;
+    }
+    else
+    {
+        cout << "Next time please enter 'y' or 'n'..." << endl;
+    }
 }
 void question4() 
 {
+    //Get random numbers
+    int seed, num1, num2, answer, userAnswer;
+    const int MAX = 100;
+    cout << "Welcome to the math quiz, enter and whole number to start: ";
+    cin >> seed;
+    srand(seed);
+    num1 = (rand() % MAX);
+    num2 = (rand() % MAX);
+    answer = num1 + num2;
+    cout << "What is " << num1 << "+" << num2 << "? ";
+    cin >> userAnswer;
+    if (answer == userAnswer)
+    {
+        cout << "That is correct!" << endl;
+    }
+    else
+    {
+        cout << "That is incorrect. Try again!" << endl;
+    }
     
 }
 
@@ -84,5 +171,5 @@ int main()
     //question1();
     //question2();
     //question3();
-    //question4();
+    question4();
 }
